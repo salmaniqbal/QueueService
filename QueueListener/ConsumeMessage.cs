cb8aed6d2cca4fce2aac01669f29aa6e91d3c3d0
Salman Iqbal <salman.iqbal1@yahoo.com>
2014-03-06 21:54:44 +0000

    first commit

---
+++ QueueListener/ConsumeMessage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure;
+
+namespace QueueListener
+{
+    public class ConsumeMessage
+    {
+        static void Consume()
+        {
+            var connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+
+           // var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+
+            var client = QueueClient.CreateFromConnectionString(connectionString, "TaskQueue");
+
+            client.Receive();
+
+            while (true)
+            {
+                var message = client.Receive();
+
+                if (message != null)
+                {
+                    try
+                    {
+                        var test = message.GetBody<string>();
+                        var messageId = message.MessageId;
+                        var keyValue1 = message.Properties["QueueMessageKey"];
+                        var taskMessageKey = message.Properties["TaskMessageKey"];
+
+                    }
+                    catch (Exception ex)
+                    {
+                        message.Abandon();
+                    }
+                }
+            }
+            
+        }
+    }
+}
---
+++ Queues/SoulQueues.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure;
+
+namespace Queues
+{
+    public class SoulQueues
+    {
+        static void Main()
+        {
+            var connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+
+            if (!namespaceManager.QueueExists("TaskQueue"))
+            {
+                namespaceManager.CreateQueue("TaskQueue");
+            }
+
+            var message = new BrokeredMessage();
+
+            message.Properties["Task"] = "Bring Some Milk";
+            //message.Properties["TaskMessageKey"] = "Message02";
+
+            var client = QueueClient.CreateFromConnectionString(connectionString, "TaskQueue");
+
+            client.Send(message);
+
+        }
+    }
+}
